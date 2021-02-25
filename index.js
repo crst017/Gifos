7fef1bf57951b26364ee7e073cf9a83c67e782da
cristian navarro leon <cristiannl.0117@gmail.com>
2021-02-25 02:23:51 -0500

    gifs search finished

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-console.log("enlazado");
-
-// tuLKngOFWAfJbQBfXEDFpS0Qnr9ndXcH
+const pTrends = document.querySelector(".trends");
+const h2SearchedTerm = document.querySelector(".searched-term");
+const imgTrends = document.querySelectorAll(".trending-gif");
+const input = document.querySelector(".input-search");
+const ulAutocomplete = document.querySelector(".ul-autocomplete");
+const spanAutocompleteDivisor = document.querySelector(".autocomplete-divisor");
+const searchButton = document.querySelector(".search-button");
+const gifsResultContainer = document.querySelector(".gifs-result-container");
 
 let url = "https://api.giphy.com/v1";
 let api_key = "tuLKngOFWAfJbQBfXEDFpS0Qnr9ndXcH";
@@ -8,13 +13,16 @@ let api_key = "tuLKngOFWAfJbQBfXEDFpS0Qnr9ndXcH";
 let urlTrendingTerms = `${url}/trending/searches?api_key=${api_key}`;
 let urlTrendingGifs = `${url}/gifs/trending?api_key=${api_key}&limit=3`;
 let urlAutocomplete = "";
-
-const pTrends = document.querySelector(".trends");
-const imgTrends = document.querySelectorAll(".trending-gif");
-const input = document.querySelector(".input-search");
+let urlSearch = "";
 
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1)
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.lastChild);
+    }
 }
 
 async function fetchTrendingTerms() {
@@ -55,8 +63,23 @@ async function fetchAutocomplete(url) {
     return name;
 }
 
-(async function () { 
+async function fetchSearch(input) {
+    
+    urlSearch = `${url}/gifs/search?api_key=${api_key}&q=${input}&limit=12`;
+    
+    const promiseSearch = await fetch(urlSearch);
+    const jsonSearch = await promiseSearch.json();
+    const gifs = jsonSearch.data;
+    let src = [];
 
+    for (const gif of gifs) {
+        src.push(gif.images.original.webp);
+    }  
+    return src;
+}
+
+(async function () { 
+    
     const resultado = await Promise.all( [ fetchTrendingTerms(), fetchTrendingGifs() ] );
     const src = resultado[1];
 
@@ -68,20 +91,54 @@ async function fetchAutocomplete(url) {
 })();
 
 input.addEventListener("input", () => {
-    let q = input.value;
-    urlAutocomplete = `${url}/gifs/search/tags?api_key=${api_key}&q=${q}&limit=4`;
 
+    let inputValue = input.value;
+    urlAutocomplete = `${url}/gifs/search/tags?api_key=${api_key}&q=${inputValue}&limit=4`;
+    
     fetchAutocomplete(urlAutocomplete).then( result => {
-        console.log(result)
 
-        // Crear una lista desordenada y añadirla como hijo del input
+        //Adds or removes the span divisor in the input box
+        if (inputValue.length > 0 && result.length != 0) {
+            spanAutocompleteDivisor.classList.add("d-inline-block");
+        }
+        else {
+            spanAutocompleteDivisor.classList.remove("d-inline-block");
+        };
+        
+        removeAllChildNodes(ulAutocomplete);
 
-        // Calcular la longitud del resultado
-        // Dentro de un ciclo crear la cantidad de list items que devuelve el resultado
-        // Asignar al list item el valor deseado ( resultado de autocompletar )
-        // Colocar en negrilla las letras que coinciden con el valor del input
-        // Hacer append del list item al listado UL
+        // Creates and appends a list item to the "ul" filled with the autocomplete fetch
+        for (let index = 0; index < result.length; index++) {
+
+            let li = document.createElement("li");
+            li.textContent = result[index];
+            ulAutocomplete.appendChild(li);
+        }
     }
     );
 });
 
+searchButton.addEventListener("click", () => {
+
+    //Removes autocomplete element and divisor, clears gifs container
+
+    removeAllChildNodes(ulAutocomplete);
+    removeAllChildNodes(gifsResultContainer);
+    spanAutocompleteDivisor.classList.remove("d-inline-block");
+
+    let inputValue = input.value;
+    h2SearchedTerm.textContent = inputValue.capitalize();
+
+    fetchSearch(inputValue).then( result => {
+
+        for (const src of result) {
+
+            let img = document.createElement("img");
+            img.src = src;
+            gifsResultContainer.appendChild(img)
+        }
+    });
+});
+
+
+// searchButton.classList.add("search-button-close");
